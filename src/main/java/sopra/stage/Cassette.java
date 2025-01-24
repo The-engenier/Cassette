7a26c46a5e3c0f8bf08a067686b54b122a1517cd
Scooby <sahebettabaaahmed@gmail.com>
2025-01-24 14:24:16 +0100

    nice

---
+++ src/main/java/sopra/stage/Cassette.java
@@ -0,0 +1,74 @@
+package sopra.stage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Cassette {
+
+    //the data and table ARRAY LIST
+    static ArrayList<Integer> data = new ArrayList<>();
+    static ArrayList<List<Integer>> table = new ArrayList<java.util.List<Integer>>();
+
+
+    // Cassette data will be printed
+    public static void Cassette(){
+        readCassette();
+    }
+
+    private static void readCassette() {
+        File file = new File("cassette.txt");
+        String line = null;
+        // line 23 to 26 will get all the data from cassette.txt
+        try (Scanner allofme = new Scanner(file)) {
+            while (allofme.hasNextLine()) {
+                line = allofme.nextLine();
+                System.out.println(line);
+
+                String sstring = "";
+                int count = 0;
+
+                // will count the lenght of the every line
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ',') {
+                        count++;
+                    }
+                }
+                // will remove every "," in the data so it can get in the list
+                for (int i = 0; i < count; i++) {
+                    sstring = line.split(",")[i];
+                    data.add(Integer.parseInt(sstring));
+
+                    if (data.size() == 800  ){
+                        table.add(data.subList(200, data.size()));
+                        data.removeAll(data.subList(-200, data.size()));
+                        break;
+                    }
+                    }
+            }
+        } catch (Exception e) { // DO NOT REMOVE
+            System.out.println(e.getMessage());
+
+        }
+
+
+
+        System.out.print("data: ");
+        System.out.println(data);
+        System.out.println(data.size());
+        System.out.print("table: ");
+        System.out.println(table);
+        System.out.println(table.size());
+    }
+    
+    private void printCassetteData()  {
+
+    }
+
+}
+
+
+    
+    
+
+
---
+++ src/main/java/sopra/stage/Main.java
@@ -0,0 +1,58 @@
+package sopra.stage;
+import java.io.IOException;
+import java.util.Scanner;
+import sopra.stage.Cassette;
+
+
+public class Main {
+
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_ORANGE ="\u0001B[38,2,255,165;";
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Cassette cassette = new Cassette();
+
+
+
+        String nombre;
+        while (true) {
+            System.out.println("select your list of options:");
+            System.out.println("1: READ");
+            System.out.println("2: Write");
+            System.out.println("3: Remove");
+            System.out.println("4: About");
+            System.out.println("5: Quit");
+
+            nombre = scanner.nextLine();
+            //switch case
+            switch (nombre){
+                case ("1"):
+                    System.out.println("read");
+                    Cassette.Cassette();
+                    break;
+                case ("2"):
+                    System.out.println("WRITE");
+                    break;
+                case ("3"):
+                    System.out.println("REMOVE");
+                    break;
+                case ("4"):
+                    System.out.println("Current Version " + ANSI_RED + "ALPHA\n" + ANSI_WHITE);
+                    System.out.println("\u001B[38;2;255;165;0mJAVA 17.0.14\u001B[0m");
+                    System.out.println("Par Ahmed\n");
+                    System.out.println("Source:");
+                    System.out.println("W3SCHOOL");
+                    System.out.println("STACKOVERFLOW");
+                    System.out.println("ORACLE DOCS\n");
+                    break;
+                case ("5"):
+                    scanner.close();
+                    System.exit(0);
+
+            }
+
+        }
+    }
+}
